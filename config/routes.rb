39ead3ad61e5cb7000506937e2d82563736a1ee1
Saskia Menke <menkesaskia@web.de>
2018-08-07 11:24:47 +0200

    task manager not pretty yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
+
+  # CREATE
+  # get 'tasks/new', to: 'tasks#new' -> new_task
+  # post 'tasks', to: 'tasks#create'
+
+  # READ
+  # get 'tasks', to: 'tasks#index', as: :all_tasks -> tasks
+  # get 'tasks/:id', to: 'tasks#show', as: :task -> task
+
+  # UPDATE
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task -> edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # DELETE
+  # delete 'tasks/:id', to: 'tasks#destroy', as: :delete_task
 end
+
+
